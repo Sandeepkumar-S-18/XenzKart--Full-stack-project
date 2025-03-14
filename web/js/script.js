8ef610dcb4a2799b291cd35de305519da48c866a
Sandeepkumar S <sandeep6361460@gmail.com>
2025-03-14 14:58:52 +0530

    alter the login page

---
+++ web/js/script.js
@@ -0,0 +1,5 @@
+function displaySignUp()
+{
+    let loginDiv = document.getElementById("login-box").style.display = "none";
+    let sigupDiv = document.getElementById("signup-box").style.display = "block";
+}
