f81417bc5306a59225da0d3d8a53d864a6ca8ed1
Sandeepkumar S <sandeep6361460@gmail.com>
2025-08-27 19:00:00 +0530

    Update the password in sql URL

--- src/java/com/MyDb/DbConnector.java
+++ src/java/com/MyDb/DbConnector.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class DbConnector 
 {
-    public static String url = "jdbc:mysql://localhost:3306/XenzKart";
+    public static String url = "jdbc:mysql://localhost:3306/XenzKart?useSSL = False";
     public static String user = "root";
-    public static String password = "";
+    public static String password = "sandeep";
     
     public static Connection connect()
     {
@@ -16,9 +16,14 @@ public class DbConnector
         
         try
         {
+            Class.forName("com.mysql.jdbc.Driver");
             con = DriverManager.getConnection(url, user, password);
         }
         catch (SQLException ex)
+        {
+            System.out.println("Not able to connect to database");
+        }
+        catch (ClassNotFoundException ex) 
         {
             System.out.println("Not able to connect to database");
         }
